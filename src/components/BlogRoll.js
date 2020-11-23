f0f828ed85eeafba44de4051b61c477aae37e9d0
Saigrog <30744074+Saigrog@users.noreply.github.com>
2020-11-23 22:29:37 +0000

    feat: use loadable component import in BlogRoll

--- src/components/BlogRoll.js
+++ src/components/BlogRoll.js
@@ -1,7 +1,9 @@
+import loadable from '@loadable/component'
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Link, graphql, StaticQuery } from 'gatsby'
-import PreviewCompatibleImage from './PreviewCompatibleImage'
+
+const PreviewCompatibleImage = loadable(() => import('./PreviewCompatibleImage'))
 
 class BlogRoll extends React.Component {
   render() {
